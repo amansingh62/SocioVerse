181fdb4c7db53f1fb207c524dd6f8e7b68ec2a42
Aman Singh <coolaman6299@gmail.com>
2026-03-01 02:13:06 +0530

    Added redis configurationn

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import { app } from "./app.js";
 import { env } from "./config/env.js";
-
+import "./lib/redis.js";
 const server = http.createServer(app);
 
 server.listen(env.PORT, () => {
