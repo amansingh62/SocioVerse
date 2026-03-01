04d4cc2e793adb8a018a172e59d3f3cff552b6d0
Aman Singh <coolaman6299@gmail.com>
2026-03-01 17:31:30 +0530

    Implemented axios interceptor for handling refresh token

---
+++ frontend/app/lib/axios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL!,
+    withCredentials: true
+});
+
+api.interceptors.response.use((res) => res, async (error) => {
+    const originalRequest = error.config;
+
+    if(error.response?.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        try {
+            await api.post("/auth/refresh");
+            return api(originalRequest);
+        } catch {
+            window.location.href = "/login";
+        }
+    }
+
+    return Promise.reject(error);
+});
+
